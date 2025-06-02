51064971f78aa83fad4a04fa83f32c654076a443
Otavio Mascarenhas <otaviomascarenhaspessoal@gmail.com>
2025-06-02 02:53:50 +0000

    fix: email endpoint

--- api/routes/email_routes.py
+++ api/routes/email_routes.py
@@ -6,7 +6,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-router = APIRouter(prefix="/api/email", tags=["email"])
+router = APIRouter(prefix="/email", tags=["email"])
 
 class InvitationEmailRequest(BaseModel):
     to_email: EmailStr
